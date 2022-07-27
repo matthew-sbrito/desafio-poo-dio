43102d5d5972c28f22c61d1903224157773f497c
matthew-sbrito <matheusbr032@gmail.com>
2022-07-27 20:21:49 -0300

    feat: add domain Course and Mentoring

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import br.com.dio.desafio.domain.Course;
+import br.com.dio.desafio.domain.Mentoring;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Course firstCourse = new Course();
+        firstCourse.setTitle("Java course");
+        firstCourse.setDescription("Java course description");
+        firstCourse.setWorkload(8);
+
+        Course secondCourse = new Course();
+        secondCourse.setTitle("JS course");
+        secondCourse.setDescription("JS course description");
+        secondCourse.setWorkload(8);
+
+        Mentoring mentoring = new Mentoring();
+        mentoring.setTitle("Java mentoring");
+        mentoring.setDescription("Java mentoring description");
+        mentoring.setDate(LocalDate.now());
+
+
+        System.out.println(firstCourse);
+        System.out.println(secondCourse);
+    }
+}
